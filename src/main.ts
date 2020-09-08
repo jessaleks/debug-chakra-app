d7c648a0fc917f25ae4f1c4ef4c23c5bdd54c95c
Aleksander Jess <jess.aleksander@gmail.com>
2020-09-08 12:17:18 +0200

    still doesn't work :/

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import Chakra, { CThemeProvider, CReset } from '@chakra-ui/vue';
+import * as customTheme from './custom-theme';
 
-Vue.config.productionTip = false
+Vue.use(Chakra, {
+	extendTheme: customTheme,
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	render: (h) => h(CThemeProvider, [h(CReset), h(App)]),
+}).$mount('#app');
